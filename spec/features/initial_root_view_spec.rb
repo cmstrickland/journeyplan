64bb2677f9d1e336952524526346937ed1f74f26
cms <cms@beatworm.co.uk>
2016-04-05 20:40:26 +0100

    Better editable negotiators
    render negotiators names in negotiator index
    makes the app slightly more manageable
    extend checks for inital / view
    moves notice to layout
    feature test for fresh system involves visit /
    should redirect to new negotiator
    add negotiator via capybara
    vist / should not redirect

--- spec/features/initial_root_view_spec.rb
+++ spec/features/initial_root_view_spec.rb
@@ -4,5 +4,12 @@ RSpec.feature 'First visit to the home page' , type: :feature do
   it "prompts to create negotiators when there are none" do
     visit "/"
     expect(page).to have_content("New Negotiator")
+    expect(page).to have_css "p#notice", text: /\S+/
+    fill_in "Username", with: Faker::Internet::user_name
+    find('#create_negotiator').click
+    # now we can visit / and we should end up at new journey
+    visit "/"
+    expect(page).to have_css "p#notice", text: /^$/
+    expect(page).to have_css "form#new_journey"
   end
 end
--- spec/views/negotiators/index.html.erb_spec.rb
+++ spec/views/negotiators/index.html.erb_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "negotiators/index", type: :view do
+  let(:negotiators) { [
+                        Fabricate(:negotiator),
+                        Fabricate(:negotiator),
+                      ] }
+
   before(:each) do
-    assign(:negotiators, [
-             Fabricate(:negotiator),
-             Fabricate(:negotiator),
-    ])
+    assign(:negotiators, negotiators)
   end
 
   it "renders a list of negotiators" do
     render
+    negotiators.each do |negotiator|
+      expect(rendered).to have_content(negotiator.username)
+    end
   end
 end
